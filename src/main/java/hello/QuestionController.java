92cb5cd8b649d32d611babd43bf2397884d18056
HanfeiDeng <TonyDeng@HanfeiDengs-MacBook-Pro.local>
2018-10-30 12:04:27 -0500

    added error control

--- src/main/java/hello/QuestionController.java
+++ src/main/java/hello/QuestionController.java
@@ -1,5 +1,6 @@
 package hello;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import hello.service.question_codeService;
+
 import hello.model.question_code;
 
 @Controller
@@ -18,12 +20,21 @@ public class QuestionController {
 	@Autowired
     private question_codeService question_codeService;
     @RequestMapping(value = "/code_test", method = RequestMethod.GET)
-    public @ResponseBody String getQuestion(
+    public @ResponseBody question_code getQuestion(
                 @RequestParam("id") long id) {
-            question_code hello=question_codeService.findbyid(id);
+            question_code hello=new question_code();
+   
+            
+            if(id>100) {
+            	hello.setId((long) 1);
+            	hello.setLanguage("outofbound");
+            	hello.setSource_code("outofbound");
+            	return hello;
+            }
+            hello=question_codeService.findbyid(id);
             // Process the request
             // Prepare the response string
-            return hello.getSource_code();
+            return hello;
     }
     @RequestMapping(value = "/code_test", method = RequestMethod.POST)
 	@ResponseBody
