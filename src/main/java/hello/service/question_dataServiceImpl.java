cfaffcf09a18b1fd5662fa9246718d1a5ae794c1
HanfeiDeng <TonyDeng@HanfeiDengs-MacBook-Pro.local>
2018-10-30 13:59:08 -0500

    added save without CRUD

---
+++ src/main/java/hello/service/question_dataServiceImpl.java
@@ -0,0 +1,31 @@
+package hello.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import hello.model.question_code;
+import hello.model.question_data;
+import hello.repository.question_dataDao;
+
+@Service
+public class question_dataServiceImpl implements question_dataService {
+
+	@Autowired
+    private question_dataDao question_dataDao;
+	
+	@Override
+	public void save(question_data mq) {
+		// TODO Auto-generated method stub
+		question_dataDao.insertquestion(mq.getTitle(),mq.getArticle());
+		//question_dataDao.saveAndFlush(mq);
+	}
+	@Override
+	public question_data findbyid(long id) {
+		return question_dataDao.findbyid(id);
+	}
+
+
+	
+}
